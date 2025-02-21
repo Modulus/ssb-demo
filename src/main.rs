26a3bad605b6194b28c3a5243a6cf7eedbfa7cb8
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2025-02-21 14:29:57 +0100

    WOrking example with request

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,15 @@ fn main() {
     println!("{:?}", df);
 
 
+    let url = "https://data.ssb.no/api/v0/dataset/1088.csv?lang=no";
+    let body = get(url).unwrap().text().unwrap();
+    let reader = Cursor::new(body);
+
+    let df = CsvReadOptions::default()
+    .with_infer_schema_length(None)
+    .with_has_header(true)
+    .with_parse_options(CsvParseOptions::default().with_try_parse_dates(true).with_separator(";".as_bytes()[0]).with_decimal_comma(true))
+    .into_reader_with_file_handle(reader).finish().unwrap();
+
+    println!("{:?}", df);
 }
