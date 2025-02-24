bfea10842bdfd146492219eb1c13d2e72badc912
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2025-02-24 14:08:26 +0100

    Added rtest and a few inbecile test cases to cover any legal input

--- src/utils/date.rs
+++ src/utils/date.rs
@@ -1,3 +1,5 @@
+
+
 use crate::types::date::YearMonth;
 
 //TODO: return result here, or option
@@ -14,13 +16,19 @@ pub fn extract_year_month_from_string(date: &str) -> YearMonth {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn test_extract_year_month_from_string_has_valid_input_date_from_ssb_data() {
-        let date = "2025M01";
+    #[rstest]
+    #[case("2025M01", 2025, 1)]
+    #[case("2023M11", 2023, 11)]
+    #[case("1917M03", 1917, 3)]
+    #[case("1658M12", 1658, 12)]
+    #[case("22M01", 22, 1)]
+    #[case("01M07", 1, 7)]
+    fn test_extract_year_month_from_string_has_valid_input_date_from_ssb_data(#[case] date: &str, #[case] year: u32, #[case] month: u32) {
         let year_month = extract_year_month_from_string(date);
-        assert_eq!(year_month.year, 2025);
-        assert_eq!(year_month.month, 1);
+        assert_eq!(year_month.year, year);
+        assert_eq!(year_month.month, month);
     }
 
 }
